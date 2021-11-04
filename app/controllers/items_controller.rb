726171db45f02f608707485ea3535fea350f4fa4
Joshua Allford <joshallfordnanook@gmail.com>
2021-11-04 16:54:22 +0000

    commit tree abea77047153efca0e8314c618cbe3c2f233cf60

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,14 @@
 class ItemsController < ApplicationController
-
-  def index
-    items = Item.all
-    render json: items, include: :user
-  end
-
+  def index 
+    user = User.find_by(id: params[:user_id])
+    
+    if user
+      render json: user.items
+    else if params[:user_id]
+      render json: { error: "User not found"}, status: :not_found
+    else
+       items = Item.all
+       render json: items, include: :user
+    end 
+  end 
 end
